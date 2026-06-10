c66af432121248c2be6004c7404c6879b263e9b4
Tirta Santoso <tirta.santoso@smardt.com>
2026-06-10 12:01:27 +0800

    update main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from rich import print
 
 from clients.gemini import generate_gemini_content, generate_gemini_image
+from clients.zai_glm import generate_glm_content, generate_glm_file_list
 from clients.deepseek import generate_deepseek_content, generate_deepseek_file_list
 
 def main():
@@ -12,6 +13,40 @@ def main():
     system_persona = './prompts/references/thronegate/_SYSTEM_PERSONA.md'
 
     prompts_list = [
+        # {
+        #     'json_premessages': './outputs/deepseek_20260606_231136_reasoning.json',
+        #     'message': './prompts/prompt22_godric_video_game.md',
+        #     'files': [
+        #         # './prompts/references/thronegate/00_MASTER_LORE.md',
+        #         # './prompts/references/thronegate/01_godric_origin.md',
+        #         # './prompts/references/thronegate/02_rogr_trauma.md',
+        #         # './prompts/references/thronegate/03_sentient_aura.md',
+        #         # './prompts/references/thronegate/04_immune_system.md',
+        #         # './prompts/references/thronegate/05_godric_intelligence.md',
+        #         # './prompts/references/thronegate/06_godric_magic.md',
+        #         # './prompts/references/thronegate/07_statjournals.md',
+        #     ],
+        # },
+        # {
+        #     'message': './prompts/prompt2_rogr_trauma.md',
+        #     'files': []
+        # },
+        # {
+        #     'message': './prompts/prompt3_gent_incident.md',
+        #     'files': []
+        # },
+        # {
+        #     'message': './prompts/prompt4_immune_system.md',
+        #     'files': []
+        # },
+        # {
+        #     'message': './prompts/prompt5_godric_intelligence.md',
+        #     'files': []
+        # },
+        # {
+        #     'message': './prompts/prompt6_godric_magic.md',
+        #     'files': []
+        # },
         {
             'message': './prompts/prompt7_statjournals.md',
             'files': [
@@ -33,7 +68,9 @@ def main():
         # },
         {
             'message': './prompts/prompt8_superpowers.md',
-            'files': []
+            'files': [
+                './prompts/references/thronegate/08_superpowers.md',
+            ]
         },
         {
             'message': './prompts/prompt9_pleasurable_stats.md',
@@ -65,15 +102,23 @@ def main():
         # },
     ]
 
-    generate_deepseek_content(
-        system_persona=system_persona,
-        prompts=prompts_list,
-        reasoning_effort='high',
-    )
+    # generate_deepseek_content(
+    #     system_persona=system_persona,
+    #     prompts=prompts_list,
+    #     reasoning_effort='high',
+    #     with_json_output=True,
+    # )
 
     # generate_deepseek_file_list(
     #     prompt=prompts_list[0],
     # )
+
+    generate_glm_content(
+        system_persona=system_persona,
+        prompts=prompts_list,
+        reasoning_effort='high',
+        with_json_output=True,
+    )
 
     # generate_gemini_content(
     #     system_persona=system_persona,
